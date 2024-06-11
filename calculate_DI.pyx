1d5a8889d6d145d4cb1419886f54285a075afcbd
shailajaAkella <68871896+shailajaAkella@users.noreply.github.com>
2024-06-10 20:56:55 -0700

    Added python version of directed information

---
+++ calculate_DI.pyx
@@ -0,0 +1,157 @@
+import math
+import numpy as np
+from scipy.spatial.distance import pdist, squareform
+
+
+class directed_information:
+
+    def __init__(self, multiplier_X=None, multiplier_Y=None, memory=5, alpha=1.01):
+
+        """
+        :param memory: total length of past history to consider
+        :param multiplier_X: multiplier to adjust kernel width of the Gram matrix for X
+        :param multiplier_Y: multiplier to adjust kernel width of the Gram matrix for Y
+        :param alpha: order of entropy
+        """
+
+        self.multiplier_X = multiplier_X
+        self.multiplier_Y = multiplier_Y
+        self.alpha = alpha
+        self.memory = memory
+        self.di = None
+
+    def GaussMat(self, mat, mult=None):
+
+        """
+        Calculates the Gram matrix using Gaussian kernel evaluates the distances between input samples after projection to RKHS
+        :param mat: input matrix
+        :param mult: Multiplier to adjust kernel width of the Gram matrix for matrix
+        :return: Gram matrix
+        """
+
+        # Ensure mat is 2-dimensional
+        if len(mat.shape) == 1:
+            mat = mat.reshape(-1, 1)
+
+        # Get the dimensions of mat
+        N, T = mat.shape
+
+        # Calculate bandwidth using Silverman's rule
+        if mult:
+            sig = (mult) * (1.06 * np.nanstd(mat)) * (N ** (-1 / 5))
+        else:
+            sig = (1.06 * np.nanstd(mat)) * (N ** (-1 / 5))
+
+        # scott's rule: sig = N**(-1/(4+T))
+
+        # Calculate pairwise squared Euclidean distances
+        pairwise_sq_dists = squareform(pdist(mat, 'sqeuclidean'))
+
+        # Calculate gram matrix using Gaussian kernel
+        return np.exp(-pairwise_sq_dists / sig ** 2)
+
+    def alpha_entropy(self, mat, mult=None):
+        """
+        Calculates entropy
+
+        :param mat: input matrix
+        :param mult: Multiplier to adjust kernel width of the Gram matrix for matrix
+        :return: entropy
+        """
+
+        # Get Gram matrices
+        N = len(mat)
+        K = self.GaussMat(mat, mult=mult) / N
+
+        # entropy estimation using the eigen spectrum
+        L = np.linalg.eigvalsh(K)
+        absL = np.abs(L)
+        H = (1 / (1 - self.alpha)) * math.log2(np.min([np.sum(absL ** self.alpha), 0.9999]))
+
+        return H
+
+    def joint_alpha_entropy(self, mat1, mat2, mat3=None, mult1=None, mult2=None, mult3=None):
+        """
+        Calculates joint alpha entropy between two or three variables
+        :param mat1: input matrix1
+        :param mat2: input matrix2
+        :param mat3: input matrix3
+        :param mult1: Multiplier to adjust kernel width of the Gram matrix for matrix1
+        :param mult2: Multiplier to adjust kernel width of the Gram matrix for matrix2
+        :param mult3: Multiplier to adjust kernel width of the Gram matrix for matrix3
+        :return: joint entropy
+        """
+
+        if mat3 is None:
+            mat3 = []
+
+        # Get gram matrices
+        N = len(mat1)
+        K1 = self.GaussMat(mat1, mult=mult1) / N
+        K2 = self.GaussMat(mat2, mult=mult2) / N
+
+        # Calculate Hadamard product
+        if len(mat3) == 0:
+            prodK = K1 * K2 * N
+        else:
+            K3 = self.GaussMat(mat3, mult=mult3) / N
+            prodK = K1 * K2 * K3 * (N ** 2)
+
+        # calculate joint alpha entropy using the eigen-spectrum
+        L = np.linalg.eigvalsh(prodK)
+        absL = np.abs(L)
+        H = (1 / (1 - self.alpha)) * math.log2(np.min([np.sum(absL ** self.alpha), 0.9999]))  # joint entropy
+
+        return H
+
+    def conditional_mi(self, x_past, y_past, y_present):
+        """
+        Calculates the conditional mutual information at time point i,
+        I(Xpast; Ypresent | Ypast) = H(Xpast, Ypast) + H(Ypresent, Ypast) - H(Xpast, Ypresent, Ypast) - H(Ypast)
+
+        :return: conditional mutual information
+        """
+
+        # H(Ypast)
+        H_ypast = self.alpha_entropy(y_past, mult=self.multiplier_Y)
+
+        # H(Xpast, Ypast)
+        H_xpast_ypast = self.joint_alpha_entropy(x_past, y_past, mult1=self.multiplier_X, mult2=self.multiplier_Y)
+
+        # H(Ypresent, Ypast)
+        H_ypresent_ypast = self.joint_alpha_entropy(y_present, y_past, mult1=self.multiplier_Y, mult2=self.multiplier_Y)
+
+        # H(Xpast, Ypresent, Ypast)
+        H_xyyp = self.joint_alpha_entropy(x_past, y_present, y_past, mult1=self.multiplier_X, mult2=self.multiplier_Y,
+                                          mult3=self.multiplier_Y)
+
+        return H_ypresent_ypast  + H_xpast_ypast - H_xyyp - H_ypast
+
+    def DI(self, X, Y):
+        """
+        calculates directed information from (X -> Y)
+        :param X: causal time series
+        :param Y: effect time series
+        :return directed information
+        """
+
+        di = 0
+        T = len(X)
+
+        for m in range(1, self.memory):
+
+            # initialize
+            x_past, y_past, y_present = np.zeros((T - m, m)), np.zeros((T - m, m)), np.zeros(T - m)
+
+            # gather past and present instances
+            for t in range(T - m):
+                x_past[t] = X[t:t + m]
+                y_past[t] = Y[t:t + m]
+                y_present[t] = Y[t + m]
+
+            # evaluate conditional mutual information and add to DI
+            di += self.conditional_mi(x_past, y_past, y_present)
+
+        self.di = di
+
+        return self.di
---
+++ utils.py
@@ -0,0 +1,121 @@
+import numpy as np
+import pandas as pd
+
+
+def simulate(num_units, num_trials, duration, step_size, weights, inp_curr, delay = 5, params=None):
+
+    """
+    The Izhikevich model can be represented through a 2 - D system of differential equations:
+
+    C * dvdt = k(v - vr)(v - vt) - u + I
+    dudt = a * (b * (v - vr) - u)
+
+    Outputs
+    u: Represents membrane recovery variable
+    v: Represents membrane potential of the neuron
+
+    Input parameters
+    a: Time scale of the recovery variable
+    ub: Sensitivity of u to the fluctuations in v
+    c: After - spike reset value of v
+    d: After - spike reset value of u
+    I: steady input current value supplied to the neuron
+    k: Related  to neuron's rheobase and input resistance
+
+    with conditions:
+        if v >= vpeak(35mV), then v = c and u = u + d
+
+    for RS neurons:
+        (a, b) = (0.03, -2)
+        (c, d) = (-50, 100)
+
+    :param num_units: total number of units in the network
+    :param num_trials: total number of trials to simulate
+    :param duration: duration of each trial in seconds
+    :param step_size: sampling frequency
+    :param weights: 2d matrix summarizing strength of connection between all neurons
+                    columns are causal variables, rows are effect variables
+    :param inp_curr: steady input current supplied to each unit
+    :param delay: time taken for input from causal neuron to arrive at the effect neuron
+    :param params: a, b, c, d, k, C
+    :return: dataframe of spike times, index represents neuron number
+    """
+
+    df = pd.DataFrame(index=np.arange(num_units))
+    df['spike_times'] = [[] for _ in range(num_units)]  # Initialize 'spike_times' column
+
+    if not params:
+        params = {n_unit: {'a': 0.03, 'b': -2, 'c': -50, 'd': 100, 'k': 0.7, 'C': 100} for n_unit in range(num_units)}
+
+    for trial in range(num_trials):
+        times = update_trial(num_units, weights, duration, step_size, inp_curr, params, delay)
+        for n_unit in range(num_units):
+            df.loc[n_unit, 'spike_times'].append(times[n_unit])
+
+    return df
+
+
+def update_trial(num_units, weights, duration, step_size, inp_curr, params, delay):
+    """
+    Evaluates trial wise updates
+    """
+
+    vr = -60
+    vt = -40
+    vpeak = 35
+    I_max = 350
+
+    T = int(duration / step_size)
+    step_size = 1
+    V = vr * np.ones((num_units, T))
+    U = np.zeros_like(V)
+    # I = np.array([inp_curr[i] * np.random.rand(T) for i in range(num_units)])
+    I = np.array([inp_curr[i] + 70 * np.random.randn(T) for i in range(num_units)])
+    R = np.zeros((num_units, T))
+    totI = I[0]
+
+    spike_times = {n_unit: [] for n_unit in range(num_units)}
+
+    for t in range(T - 1):
+        units_fired = []
+        for n_unit in range(num_units):
+            v, u, fired = izhikevich_cell(V[n_unit, t], U[n_unit, t],
+                                          totI[n_unit], step_size, vpeak, vr, vt, params[n_unit])
+            if fired:
+                V[n_unit, t], V[n_unit, t + 1], U[n_unit, t + 1] = vpeak, params[n_unit]['c'], u + params[n_unit]['d']
+                units_fired.append(n_unit)
+                spike_times[n_unit].append(t * step_size)
+            else:
+                V[n_unit, t + 1], U[n_unit, t + 1] = v, u
+
+        units_fired = np.array(units_fired)
+        if len(units_fired) > 0:
+            t_end = np.min([t + delay, T])
+            R = R + np.hstack((np.zeros((num_units, t_end)),
+                               np.sum(weights[:, units_fired]*I_max, axis=1)[:, np.newaxis] * np.ones(
+                                   (num_units, min([delay + 1, T - t_end]))),
+                               np.zeros((num_units, max(0, T - (t + 2 * delay + 1))))))
+
+        totI = I[:, t + 1] + R[:, t + 1]
+
+    return spike_times
+
+
+def izhikevich_cell(v, u, totI, step_size, vpeak, vr, vt, params):
+
+    """
+    Model each Izhikevich cell
+    """
+
+    a = params['a']
+    b = params['b']
+    k = params['k']
+    C = params['C']
+
+    v = v + step_size * (k * (v - vr) * (v - vt) - u + totI) / C
+    u = u + step_size * a * (b * (v - vr) - u)
+    fired = 0
+    if v >= vpeak:
+        fired = 1
+
+    return v, u, fired
